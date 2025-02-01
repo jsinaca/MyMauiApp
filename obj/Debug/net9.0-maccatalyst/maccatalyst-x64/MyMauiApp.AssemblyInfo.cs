5b8580b08dce08b83e9527b7164e3f2e981b2a2b
JosueSinaca <sinaca56@gmail.com>
2025-02-01 14:34:35 -0700

    My name

--- obj/Debug/net9.0-maccatalyst/maccatalyst-x64/MyMauiApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-x64/MyMauiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ce1229e2322b7fa88000d891d5e7ee638ce23cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
